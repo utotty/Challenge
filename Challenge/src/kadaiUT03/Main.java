7fff73c5681e63eb0e016f40d72245ad53e8be9d
ruri.sakamoto <ruri.sakamoto@NTPC-PEK-001>
2015-11-09 15:14:39 +0900

    指摘反映しました

--- Challenge/src/kadaiUT03/Main.java
+++ Challenge/src/kadaiUT03/Main.java
@@ -25,7 +25,7 @@ public class Main {
 				@Override
 				public int compare(Person p1, Person p2) {
 					return p2.getLastName().compareTo(p1.getLastName())
-							& p1.getAge() - p2.getAge();
+							& p2.getAge() - p1.getAge();
 				}
 			});
 
